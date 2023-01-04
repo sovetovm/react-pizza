696e10c3a5d7f9a1276cf574b9d37363aed6ab84
SOVETOV <sovotoffm@gmail.com>
2023-01-04 21:30:01 +0300

    Добавил логику категорий.

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
 export default function Categories() {
+  const [activeIndex, setActiveIndex] = useState(3);
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li className={activeIndex === 0 ? 'active' : ''} onClick={() => onClickCategory(0)}>
+          Все
+        </li>
+        <li className={activeIndex === 1 ? 'active' : ''} onClick={() => onClickCategory(1)}>
+          Мясные
+        </li>
+        <li className={activeIndex === 2 ? 'active' : ''} onClick={() => onClickCategory(2)}>
+          Вегетарианская
+        </li>
+        <li className={activeIndex === 3 ? 'active' : ''} onClick={() => onClickCategory(3)}>
+          Гриль
+        </li>
+        <li className={activeIndex === 4 ? 'active' : ''} onClick={() => onClickCategory(4)}>
+          Острые
+        </li>
+        <li className={activeIndex === 5 ? 'active' : ''} onClick={() => onClickCategory(5)}>
+          Закрытые
+        </li>
       </ul>
     </div>
   );
