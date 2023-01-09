e9f74a7b2c0d99fc4371e2fe54394fd28e5ad6dd
SOVETOV <sovotoffm@gmail.com>
2023-01-09 16:45:26 +0300

    Поправил выбор к-ий,  работает с 1эл-том в массиве

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -17,10 +17,10 @@ export default function PizzaBlock({ title, price, imageUrl, sizes, types }) {
       <h4 className="pizza-block__title">{title} </h4>
       <div className="pizza-block__selector">
         <ul>
-          {types.map((typeId) => (
+          {types.map((typeId, index) => (
             <li
-              onClick={() => setActiveType(typeId)}
-              className={activeType === typeId ? 'active' : ''}>
+              onClick={() => setActiveType(index)}
+              className={activeType === index ? 'active' : ''}>
               {typeNames[typeId]}
             </li>
           ))}
