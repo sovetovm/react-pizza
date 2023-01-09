dc2f9ce2535df5e5e130f415236c7af6a712d7e6
SOVETOV <sovotoffm@gmail.com>
2023-01-09 16:36:07 +0300

    Сократил написание компонента пицц

--- src/App.js
+++ src/App.js
@@ -18,13 +18,7 @@ export default function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {pizzas.map((obj) => (
-              <PizzaBlock
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-                types={obj.types}
-              />
+              <PizzaBlock {...obj} />
             ))}
           </div>
         </div>
