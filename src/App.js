a9a0fb4192af206d917fce3f94da239a3e792478
SOVETOV <sovotoffm@gmail.com>
2023-01-10 16:43:22 +0300

    Добавил скелетон для PizzaBlock

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 import { useEffect, useState } from 'react';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 export default function App() {
   const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://63bd4096d6600623889f0d40.mockapi.io/items')
       .then((res) => res.json())
-      .then((json) => setPizzas(json));
+      .then((json) => {
+        setPizzas(json);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -25,9 +30,9 @@ export default function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
