e5890e96a2d367ac6c753d003ae4843bf33ab5fe
SOVETOV <sovotoffm@gmail.com>
2023-01-10 15:32:33 +0300

    Получение json пицц с бэка. useEffect

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('https://63bd4096d6600623889f0d40.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => setPizzas(json));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
