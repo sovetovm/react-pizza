6567e5d66a6053e1631541285b4180dead842da5
SOVETOV <sovotoffm@gmail.com>
2023-01-31 15:21:20 +0300

    Сделал поиск пицц через бэк

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,11 +16,14 @@ export default function Home({ searchValue }) {
 
   useEffect(() => {
     setIsLoading(true);
+
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const sortBy = sortType.sortProperty.replace('-', '');
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const search = searchValue ? `&search=${searchValue}` : '';
+
     fetch(
-      `https://63bd4096d6600623889f0d40.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://63bd4096d6600623889f0d40.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((json) => {
@@ -28,17 +31,10 @@ export default function Home({ searchValue }) {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue]);
 
   const skeletons = [...new Array(8)].map((_, index) => <Skeleton key={index} />);
-  const items = pizzas
-    .filter((obj) => {
-      if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-        return true;
-      }
-      return false;
-    })
-    .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const items = pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
   return (
     <div className="container">
