f6368025095bca52e13f742490e450e2b306552b
SOVETOV <sovotoffm@gmail.com>
2023-03-12 12:03:55 +0300

    Динамический импорт для оптимизации билда

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
 
 const App: React.FC = () => {
   return (
@@ -15,9 +16,30 @@ const App: React.FC = () => {
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/pizza/:id" element={<FullPizza />} />
-          <Route path="*" element={<NotFound />} />
+          <Route
+            path="/cart"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Cart />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/pizza/:id"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <FullPizza />
+              </Suspense>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <NotFound />
+              </Suspense>
+            }
+          />
         </Routes>
       </div>
     </div>
